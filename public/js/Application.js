dea5a51b600d520ff38587d061fd1eb75e5f94c5
Adam Timberlake <adam.timberlake@gmail.com>
2014-02-07 01:29:13 +0000

    Added initial index.html w/ Angular

---
+++ public/js/Application.js
@@ -0,0 +1,10 @@
+/**
+ * @module Tidal.js
+ * @author Adam Timberlake
+ */
+(function($window, $angular) {
+
+    // Bootstrap the Tidal.js application!
+    $window.tidalApp = $angular.module('tidalApp', []);
+
+})(window, window.angular);
