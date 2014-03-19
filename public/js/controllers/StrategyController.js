48687664c7ca275ea64c5ee8aaf9e0bfe0383192
Adam Timberlake <adam.timberlake@gmail.com>
2014-03-19 22:02:59 +0000

    Added logic for parsing events

--- public/js/controllers/StrategyController.js
+++ public/js/controllers/StrategyController.js
@@ -9,10 +9,37 @@
 
     function strategyController($scope, $http) {
 
+        /**
+         * @property strategies
+         * @type {Object}
+         */
+        $scope.strategies = {};
+
+        /**
+         * @property events
+         * @type {Array}
+         */
+        $scope.events = [];
+
+        /**
+         * @property index
+         * @type {Number}
+         * @default 1000
+         */
+        $scope.index = 1000;
+
         $http.get('../../strategies/login.yaml').then(function then(response) {
 
-            $scope.strategies = $yaml.load(response.data);
-            console.log($scope.strategies);
+            var index = (++$scope.index).toString(16);
+            $scope.strategies[index] = $yaml.load(response.data);
+            var first = $scope.strategies[index][0];
+
+            // It can only begin this strategy if it's an "on" event, otherwise it's part
+            // of a strategy that's based on the user initialisation, rather than the backend
+            // initialisation.
+            if (first.type === 'on') {
+                $scope.events[first.event] = index;
+            }
 
         });
 
