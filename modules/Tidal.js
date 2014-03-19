5d0ac0dafab661623d3a86f4b959cdfc78690044
Adam Timberlake <adam.timberlake@gmail.com>
2014-03-19 15:13:42 +0000

    Added skeleton Tidal

---
+++ modules/Tidal.js
@@ -0,0 +1,39 @@
+(function($window) {
+
+    /**
+     * @module Tidal
+     * @author Adam Timberlake
+     * @link http://github.com/Wildhoney/Tidal.js
+     * @constructor
+     */
+    function Tidal() { }
+
+    /**
+     * @property prototype
+     * @type {Object}
+     */
+    Tidal.prototype = {
+
+        /**
+         * @method addResponder
+         * @param options {Object}
+         * @return {void}
+         */
+        addResponder: function addResponder(options) {
+
+        },
+
+        /**
+         * @method addStrategy
+         * @param options {Object}
+         * @return {void}
+         */
+        addStrategy: function addStrategy(options) {
+
+        }
+
+    };
+
+    $window.tidal = new Tidal();
+
+})(window);
