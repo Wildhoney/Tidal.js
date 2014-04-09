7b3b50577007b6a0695fea79f8b06300eded30c9
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-09 22:43:30 +0100

    Added Tidal.yaml config file

--- bin/tidal.js
+++ bin/tidal.js
@@ -18,41 +18,29 @@
         path         = require('path'),
         glob         = require('glob'),
         fs           = require('fs'),
-        app          = express(),
-        ioServer     = require('socket.io').listen(options.localSocketPort);
+        app          = express();
 
-    ioServer.sockets.on('connection', function (socket) {
-
-        socket.on('user/sign-in', function (options) {
-            console.log(options);
-
-            socket.emit('user/signed-in', { loggedIn: true });
-
-        });
-
-    });
-
-    // Connect to the awaiting Socket.io server.
-    ioClient.connect('http://' + options.remoteIp + ':' + options.remotePort);
-
-    // Begin Express so the statistics are available from the `localPort`.
-    app.use(express.static(__dirname + '/..'));
-    app.listen(options.localHttpPort);
-
-    glob(__dirname + '/../strategies/*.yaml', options, function (error, files) {
-
-        var _files = [];
-
-        files.forEach(function forEach(file) {
-            file = path.basename(file);
-            _files.push(file);
-        });
-
-        fs.writeFile(__dirname + '/../strategies/strategies.conf', _files.join(','));
-
-        // Open the statistics in the new user's browser.
-        childProcess.spawn('open', ['http://localhost:' + options.localHttpPort + '/public']);
-
-    });
+//    // Connect to the awaiting Socket.io server.
+//    ioClient.connect('http://' + options.remoteIp + ':' + options.remotePort);
+//
+//    // Begin Express so the statistics are available from the `localPort`.
+//    app.use(express.static(__dirname + '/..'));
+//    app.listen(options.localHttpPort);
+//
+//    glob(__dirname + '/../strategies/*.yaml', options, function (error, files) {
+//
+//        var _files = [];
+//
+//        files.forEach(function forEach(file) {
+//            file = path.basename(file);
+//            _files.push(file);
+//        });
+//
+//        fs.writeFile(__dirname + '/../strategies/strategies.conf', _files.join(','));
+//
+//        // Open the statistics in the new user's browser.
+//        childProcess.spawn('open', ['http://localhost:' + options.localHttpPort + '/public']);
+//
+//    });
 
 })();
