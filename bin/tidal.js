678263d9fbe5ce506caf4c7208effed7a6054a38
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-09 22:51:04 +0100

    Added YAML parsing

--- bin/tidal.js
+++ bin/tidal.js
@@ -2,23 +2,15 @@
 
     "use strict";
 
-    /**
-     * @property options
-     * @type {Object}
-     */
-    var options = {
-        remoteSocket: '127.0.0.1:8888',
-        localHttpPort: 7891,
-        localSocketPort: 3001
-    };
+    var yaml = require('js-yaml'),
+        io   = require('socket.io-client'),
+        path = require('path'),
+        glob = require('glob'),
+        fs   = require('fs');
 
-    var ioClient     = require('socket.io-client'),
-        childProcess = require('child_process'),
-        express      = require('express'),
-        path         = require('path'),
-        glob         = require('glob'),
-        fs           = require('fs'),
-        app          = express();
+    var config = yaml.safeLoad(fs.readFileSync(__dirname + '/tidal.yaml', 'utf8'));
+
+    console.log(config);
 
 //    // Connect to the awaiting Socket.io server.
 //    ioClient.connect('http://' + options.remoteIp + ':' + options.remotePort);
@@ -29,17 +21,17 @@
 //
 //    glob(__dirname + '/../strategies/*.yaml', options, function (error, files) {
 //
-//        var _files = [];
+//var _files = [];
 //
-//        files.forEach(function forEach(file) {
-//            file = path.basename(file);
-//            _files.push(file);
-//        });
+//files.forEach(function forEach(file) {
+//    file = path.basename(file);
+//    _files.push(file);
+//});
 //
-//        fs.writeFile(__dirname + '/../strategies/strategies.conf', _files.join(','));
+//fs.writeFile(__dirname + '/../strategies/strategies.conf', _files.join(','));
 //
-//        // Open the statistics in the new user's browser.
-//        childProcess.spawn('open', ['http://localhost:' + options.localHttpPort + '/public']);
+//// Open the statistics in the new user's browser.
+//childProcess.spawn('open', ['http://localhost:' + options.localHttpPort + '/public']);
 //
 //    });
 
