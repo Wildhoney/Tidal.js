f9bc7cabb091ca7d0a4dae3c81c66ce71106c7d7
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-15 21:44:28 +0100

    Added compressing of releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,43 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        /**
+         * @property pkg
+         * @type {Object}
+         */
         pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * @property jshint
+         * @type {Object}
+         */
         jshint: {
             all: ['bin/**/*.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
+        },
+
+        /**
+         * @property compress
+         * @type {Object}
+         */
+        compress: {
+            main: {
+                options: {
+                    archive: 'releases/<%= pkg.version %>.zip'
+                },
+                files: [
+                    { flatten: true, src: ['bin/**'], dest: 'releases/', filter: 'isFile' }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     grunt.registerTask('test', ['jshint']);
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'compress']);
 
 };
